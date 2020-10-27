9aac9f906a275db26070dcc8380f2d39a97de34c
0kira <okira@hotmail.be>
2020-10-27 11:39:45 +0100

    Début

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,4 @@
 
-import { girocode } from "@dipser/epc-qr-code.js";
+/* import { girocode } from "@dipser/epc-qr-code.js";
 var g = girocode({ 'bic': 'PBNKDEFF', 'name': 'Postbank', 'iban': 'DE02100100100006820101', 'currency': 'EUR', 'amount': '10.00', 'reason': 'Invoice X' });
-document.querySelector('#elemid').innerHTML = g.svg();
+document.querySelector('#elemid').innerHTML = g.svg(); */
