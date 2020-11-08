e7f1839c88c5c8a80d532634571390e9c740bcd5
0kira <okira@hotmail.be>
2020-11-08 10:00:00 +0100

    one page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,3 +2,11 @@
 /* import { girocode } from "@dipser/epc-qr-code.js";
 var g = girocode({ 'bic': 'PBNKDEFF', 'name': 'Postbank', 'iban': 'DE02100100100006820101', 'currency': 'EUR', 'amount': '10.00', 'reason': 'Invoice X' });
 document.querySelector('#elemid').innerHTML = g.svg(); */
+import gsap from 'gsap';
+let arrow = document.querySelector('.js-arrow');
+
+arrow.addEventListener('click', () => {
+    gsap.to(".rainbow", {
+    });
+    console.log("coucou");
+});
