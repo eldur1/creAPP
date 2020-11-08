f7e1dc1fdc2def90b33f05ce9e370d9bb89d9d50
0kira <okira@hotmail.be>
2020-11-08 17:13:21 +0100

    burger menu fini

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,22 +1,41 @@
-
-/* import { girocode } from "@dipser/epc-qr-code.js";
-var g = girocode({ 'bic': 'PBNKDEFF', 'name': 'Postbank', 'iban': 'DE02100100100006820101', 'currency': 'EUR', 'amount': '10.00', 'reason': 'Invoice X' });
-document.querySelector('#elemid').innerHTML = g.svg(); */
 import { gsap } from 'gsap';
 import { _numWithUnitExp } from 'gsap/gsap-core';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 
+// QR Code
+import { girocode } from "@dipser/epc-qr-code.js";
 
-
+let bic = ""
+let name = ""
+let iban = ""
+var g = girocode({
+    'bic': bic,
+    'name': name, 
+    'iban': iban,
+    'currency': 'EUR',
+    'amount': '5.00',
+    'reason': 'Invoice X' 
+});
+document.querySelector('#elemid').innerHTML = g.svg();
 
 // 
 
 // Position of nav--main in the window
 let nav_main = document.querySelector(".nav-container");
 let wHeight = window.innerHeight;
+let closeMenu = document.querySelector('.link--close-menu');
+// Affiche ou non le bouton close menu
+function checkMobile() {
+    // Si mobile
+    if(window.innerWidth >= 900) {
+        closeMenu.style.display = "none";
+    }
+}
+checkMobile()
 
+// Check window size
 window.addEventListener('resize', () => {
     if(window.innerWidth > 900) {
         window.addEventListener('scroll', () => {
@@ -25,9 +44,12 @@ window.addEventListener('resize', () => {
             let dynamicOpacity = 1 - (nav_pos_top/wHeight);
             nav_main.style.opacity = dynamicOpacity;
             });
+            closeMenu.style.display = "none";
     }
     else {
         nav_main.style.opacity = 1;
+        closeMenu.style.display = "inline";
+
     }
 });
 
@@ -35,21 +57,37 @@ window.addEventListener('resize', () => {
 
 // Burger menu 
 let toggle = true;
-let burger = document.querySelector('.burger-menu');
-burger.addEventListener('click', toggleMenu);
-function toggleMenu() {
 
-    if(toggle) {
-        gsap.to(nav_main, {
-            x:"-200px"
-        });
-        toggle = false;
-    }
-    else {
-        gsap.to(nav_main, {
-            x:"0"
-        });
-        toggle = true;
-    }
+let burger = document.querySelector('.burger-menu');
+burger.addEventListener('click', () => {
+    let wWidth = window.innerWidth
+    gsap.to(nav_main, {
+        x:-wWidth
+    });
+    toggle = false;
+});
+
+function closeAnimation() {
+    let wWidth = window.innerWidth;
+    gsap.to(nav_main, {
+        x:0
+    });
+    toggle = true;
+}
+function toggleSelection() {
 
 }
+   
+function menu() {
+    closeAnimation()
+    toggleSelection()
+}
+let link_main = document.querySelectorAll('.link--main');
+for(let i=1; i < link_main.length; i++ ) {
+    let item = link_main[i];
+    item.addEventListener('click', menu)
+    if(!item.classList.contains('link--selected')) {
+        item.classList.add('.link--selected')
+    }
+}
+
